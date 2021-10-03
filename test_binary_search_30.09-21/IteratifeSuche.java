9a3eeaa9767e40560fb0099d6c689edb9830b671
Josef Strillinger <jstrillinger@tsn.at>
2021-10-03 13:55:58 +0200

    iterativ und binary

--- test_binary_search_30.09-21/IteratifeSuche.java
+++ test_binary_search_30.09-21/IteratifeSuche.java
@@ -1,7 +1,7 @@
 import java.lang.reflect.Array;
 
 public class IteratifeSuche{
-    public static void public static void main(String[] args) {
+    public static void main(String[] args) {
         int[] arr = {1,2,3,4,5,6,7,8,9};
 
     } 
---
+++ test_binary_search_30.09-21/test_30.09.21/src/iterativeAndBinary.java
@@ -0,0 +1,51 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class iterativeAndBinary {
+    public static void main(String[] args) throws Exception {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+        Arrays.sort(arr);
+        binarySearch(arr, 12);
+        iterativeSearch(arr, 12);
+    }
+
+    public static void binarySearch(int[] input, int searchNumber) {
+        int first = 0;
+        int last = input.length - 1;
+        int middle = (first + last) / 2;
+        int steps = 0;
+        
+        while (first <= last) {
+            if (input[middle] < searchNumber) {
+                 first = middle + 1;   
+                 steps++;           
+            } else if (input[middle] == searchNumber) {
+               
+                System.out.println("Number "+searchNumber+" found at location "+middle+" in "+steps+" steps");
+               break;
+            } else {
+               last = middle - 1;
+               steps++;
+            }
+  
+            middle = (first + last) / 2;
+       }
+     if (first > last) {
+     System.out.println(searchNumber 
+     + " is not present in the list.\n");
+        }
+     }
+
+     public static void iterativeSearch(int[] input, int searchNumber){
+        int steps = 0;
+        for(int i = 0; i < input.length; i++){
+            if(input[i] == searchNumber){
+                System.out.println("Number "+searchNumber+" found at lovation "+i+" in "+steps+" steps.");
+            }
+            else{
+                steps++;
+            }
+        }
+     }
+  
+}
