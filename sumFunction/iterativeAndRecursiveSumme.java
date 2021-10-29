05a0246a45f431bba07bd1895f73f9011ce95485
Josef Strillinger <jstrillinger@tsn.at>
2021-10-29 15:12:07 +0200

    1.3

--- sumFunction/iterativeAndRecursiveSumme.java
+++ sumFunction/iterativeAndRecursiveSumme.java
@@ -3,23 +3,20 @@ import java.util.Scanner;
 public class iterativeAndRecursiveSumme {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
-        System.out.print("Bitte geben Sie die Länge das Arrays ein: ");
+        System.out.print("Bitte geben Sie die gewünschte Summe ein: ");
         int r = input.nextInt();
         input.close(); 
         int iSum = iterativeSum(r);
         System.out.println("Iterative Summe");
-        System.out.println("Die Summe ist: "+i+"");
+        System.out.println("Die Summe ist: "+iSum+"");
         int rSum = recursiveSum(r);
         System.out.println("Rekursive Summe");
-        System.out.println("Die Summe ist: "+i+"");
+        System.out.println("Die Summe ist: "+rSum+"");
     }
-}
-
-
 
 public static int iterativeSum(int helpNum){
     int sum = 0;
-    for(int i = 0; i < helpNum; i++){
+    for(int i = 0; i < helpNum+1; i++){
         sum += i;
     }
     return sum;
@@ -31,4 +28,5 @@ public static int recursiveSum(int helpNum){
     }else {
         return helpNum;
     }
+}
 }
