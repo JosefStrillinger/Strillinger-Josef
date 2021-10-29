8510af797efcb54585ab68c28007bdd64e14e150
Josef Strillinger <jstrillinger@tsn.at>
2021-10-29 15:29:04 +0200

    Create recursiveExponent.java

---
+++ exponentialFunction/recursiveExponent.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class recursiveExponent {
+    public static void main(String[] args) throws Exception {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Bitte geben Sie die gewünschte Grundzahl ein: ");
+        int b = input.nextInt();
+        System.out.print("Bitte geben Sie den gewünschten Exponenten ein: ");
+        int e = input.nextInt();
+        input.close(); 
+        
+        int expon = recursiveExpo(b, e);
+        System.out.println("Rekursiver Exponent");
+        System.out.println("Die Summe ist: "+expon+"");
+    }
+
+
+public static int recursiveExpo(int helpBase, int helpExpo){	
+    if (helpExpo==0){ 
+        return 1; 
+    }else {  
+        helpBase *= recursive(helpBase, helpExpo-1); 
+    }
+    return helpBase;
+}
+}
