8cebd8a05a4aa5314f9b5c91ad8c2e717a251e22
Josef Strillinger <jstrillinger@tsn.at>
2022-03-06 18:13:05 +0100

    sort

--- bubblesort/Bubblesort.java
+++ bubblesort/Bubblesort.java
@@ -11,15 +11,24 @@ public class Bubblesort {
         input.close(); 
         int[] arr = new int[r];
         createRandomArray(arr);
+        int[] help1 = arr;
+        int[] help2 = arr;
+        int[] help3 = arr;
         //arr = IntStream.range(1, 100).toArray();
-       System.out.println("Ungeordnet:");
-       display(arr);
-       System.out.println("-------------------------------------------------------------");
-       System.out.println("Geordnet:");
-       bubblesort(arr);
-       display(arr);
-
-        
+        System.out.println("Ungeordnet:");
+        display(arr);
+        System.out.println("-------------------------------------------------------------");
+        System.out.println("Bubblesort | Geordnet:");
+        bubblesort(help1);
+        display(help1);
+        System.out.println("-------------------------------------------------------------");
+        System.out.println("Selectionsort | Geordnet: ");
+        selectionSort(help2);
+        display(help2);
+        System.out.println("-------------------------------------------------------------");
+        System.out.println("Insertionsort | Geordnet: ");
+        insertionSort(help3);
+        display(help3);
     }
 
     static void createRandomArray(int helpArr[]) {
@@ -53,6 +62,43 @@ public class Bubblesort {
         time2 = System.nanoTime();
         System.out.println("Zeit: "+(time2-time1)/1000+" ms");
 
+    }
+
+    private static void selectionSort(int helpArr[]){
+        long time1 = System.nanoTime();
+        long time2;
+        int l = helpArr.length;
+        for(int i = 0; i < l; i++){
+            int min = i;
+            for(int j = i+1; j < l; j++){
+                if(helpArr[j] < helpArr[min]){
+                    min = j;
+                }
+            }
+            int temp = helpArr[min];
+            helpArr[min] = helpArr[i];
+            helpArr[i] = temp;
+        }
+        time2 = System.nanoTime();
+        System.out.println("Zeit: "+(time2-time1)/1000+" ms");
+
+    }
+
+    private static void insertionSort(int helpArr[]){
+        long time1 = System.nanoTime();
+        long time2;
+        int n = helpArr.length;
+        for(int i = 1; i < n; i++){
+            int key = helpArr[i];
+            int j = i - 1;
+            while(j >= 0 && helpArr[j] > key){
+               helpArr[j + 1] = helpArr[j];
+                j = j-1;
+            }
+            helpArr[j+1] = key;
+        }
+        time2 = System.nanoTime();
+        System.out.println("Zeit: "+(time2-time1)/1000+" ms");
     }
 
     static void display(int helpArr[]){
